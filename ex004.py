993ce79f046bd53338ffd38e164c1a8f8c98ecdb
Alexandre Seyffert <alexseyf66@gmail.com>
2021-12-19 16:30:52 -0300

    segundo commit

---
+++ ex004.py
@@ -0,0 +1,8 @@
+var = input('Digite algo: ')
+print('Só tem espaços? ', var.isspace())
+print('É um número? ', var.isnumeric())
+print('É alfabético?', var.isalpha())
+print('É alfanumérico? ', var.isalnum())
+print('Está em maiúsculas? ', var.isupper())
+print('Está em minúsculas? ', var.islower())
+print('Está capitalizada? ', var.istitle())
---
+++ ex006.py
@@ -0,0 +1,6 @@
+import math
+
+num = int(input('Digite um número: '))
+print('O dobro de {} é {}.'.format(num, (num*2)))
+print('O triplo de {} é {}.'.format(num, (num*3)))
+print("A Raiz Quadrada de {} é {:.2f}.".format(num, (math.sqrt(num))))
